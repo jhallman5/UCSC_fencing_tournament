e8ffc6731e282d672d42546824ea5feac7c66e72
John <jhallman5@gmail.com>
2018-04-01 08:46:47 -0700

    Adds test_letter_rank

--- linked_list_test.py
+++ linked_list_test.py
@@ -5,12 +5,15 @@ class LL_test(unittest.TestCase):
     LL = None
 
     def setUp(self):
+        ''' Inits LL. '''
         self.LL = Linked_List()
 
     def test_init(self):
+        ''' Tests Initialization. '''
         self.assertEqual(self.LL.head, None, 'Initial HEAD should be None.')
 
     def test_insert(self):
+        ''' Tests linked_list method: insert. '''
         node_1 = {'first_name': ' Keith', 'last_name': 'LICHTEN', 'rank': 'A14', 'team': 'EBFG '}
         self.LL.insert(node_1)
         self.assertDictEqual(node_1, self.LL.head.data, 'Inserting 1 into empty list should create the head.')
@@ -33,7 +36,7 @@ class LL_test(unittest.TestCase):
         self.assertDictEqual(node_5, self.LL.head.data, 'Inserting an evenly ranked node should insert at the first apprpriate location.')
 
     def test_is_new_node_higher_rank(self):
-        #def is_new_node_higher_rank(self, current, new_node):
+        ''' Tests linked_list method: is_new_node_higher_rank(self, current, new_node). '''
         node_1 = Node({'first_name': ' Keith', 'last_name': 'LICHTEN', 'rank': 'A14', 'team': 'EBFG '})
         node_2 = Node({'first_name': ' Alexandre', 'last_name': 'RACHTCHININE', 'rank': 'A15', 'team': 'NO FEAR'})
         node_3 = Node({'first_name': ' Mehmet', 'last_name': 'TEPEDELENLIOGLU', 'rank': 'A15', 'team': 'EBFG '})
@@ -41,6 +44,30 @@ class LL_test(unittest.TestCase):
         self.assertFalse(self.LL.is_new_node_higher_rank(node_2, node_1), 'Should return false when evaluating a new node that is lower rank than current.')
         self.assertTrue(self.LL.is_new_node_higher_rank(node_1, node_2), 'Should return true when evaluating a new node that is higher rank than current.')
         self.assertTrue(self.LL.is_new_node_higher_rank(node_2, node_3), 'Should return true when evaluating a new node that is equal rank to current.')
+
+    def test_get_letter_rank(self):
+        ''' Tests linked_list method: get_letter_rank(self, node). '''
+        node_1 = Node({'first_name': ' Keith', 'last_name': 'LICHTEN', 'rank': 'A14', 'team': 'EBFG '})
+        self.assertEqual(self.LL.get_letter_rank(node_1), 5, 'A Node of rank A should return 5.')
+
+        node_2 = Node({'first_name': ' Alexandre', 'last_name': 'RACHTCHININE', 'rank': 'B15', 'team': 'NO FEAR'})
+        self.assertEqual(self.LL.get_letter_rank(node_2), 4, 'A Node of rank B should return 4.')
+
+        node_3 = Node({'first_name': ' Mehmet', 'last_name': 'TEPEDELENLIOGLU', 'rank': 'C15', 'team': 'EBFG '})
+        self.assertEqual(self.LL.get_letter_rank(node_3), 3, 'A Node of rank C should return 3.')
+
+        node_4 = Node({'first_name': ' Alfred', 'last_name': 'ROEBUCK', 'rank': 'D13', 'team': 'NO FEAR'})
+        self.assertEqual(self.LL.get_letter_rank(node_4), 2, 'A Node of rank D should return 2.')
+
+        node_5 = Node({'first_name': ' Tracy', 'last_name': 'COLWELL', 'rank': 'E15', 'team': 'EBFG '})
+        self.assertEqual(self.LL.get_letter_rank(node_5), 1, 'A Node of rank E should return 1.')
+
+        node_6 = Node({'first_name': ' Tomas', 'last_name': 'STRAKA', 'rank': 'U', 'team': ''})
+        self.assertEqual(self.LL.get_letter_rank(node_6), 0, 'A Node of rank U should return 0.')
+
+    # def test_get_year_rank(self):
+    #     ''' Tests linked_list method: get_year_rank(self, node). '''
+    #
 
 if __name__ == '__main__':
     unittest.main()
