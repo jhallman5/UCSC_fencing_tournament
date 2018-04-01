a78d0162fd436f9c04ad25d48db7bf92ef853d4a
John <jhallman5@gmail.com>
2018-04-01 14:58:29 -0700

    Adds print pools function

--- sort_teams.py
+++ sort_teams.py
@@ -1,15 +1,18 @@
 import sys
-from model import create_linked_list, create_init_pools
+from model import create_linked_list, create_init_pools, print_pools
 
 # First create a linked list of participates
 LL = create_linked_list(sys.argv[1])
 
 # use LL to create init_pools
 init_pools = create_init_pools(LL)
-print init_pools[0]
-print '------------'
-print init_pools[1]
-print '------------'
-print init_pools[2]
-print '------------'
-print init_pools[3]
+
+# print init_pools[0]
+# print '------------'
+# print init_pools[1]
+# print '------------'
+# print init_pools[2]
+# print '------------'
+# print init_pools[3]
+
+print_pools(init_pools)
